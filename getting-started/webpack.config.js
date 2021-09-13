fbdd7f6c54257688c96148d2be5137516c533928
gos1004 <nnv711@gmail.com>
2021-09-14 00:54:51 +0900

    웹팩 시작하기 튜토리얼 파트 4 - 웹팩 설정 파일 적용

---
+++ getting-started/webpack.config.js
@@ -0,0 +1,11 @@
+//NODE JS 문법
+var path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
